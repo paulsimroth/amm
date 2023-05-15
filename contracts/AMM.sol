cfe7397a8036b88e98d0091a91387eb407af105c
Paul Simroth <paul.simroth@proton.me>
2023-05-15 16:41:00 +0200

    addLiquidity

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -33,34 +33,96 @@ contract AMM {
         reserve1 = _reseve1;
     }
 
-    function swap(address _tokenIn, uint _amountIn) external returns(uint amountOut) {
-        require(_tokenIn == address(token0) || _tokenIn == address(token1), "Invalid token");
+    function swap(
+        address _tokenIn,
+        uint _amountIn
+    ) external returns (uint amountOut) {
+        require(
+            _tokenIn == address(token0) || _tokenIn == address(token1),
+            "Invalid token"
+        );
         require(_amountIn > 0, "amountIn = 0");
 
         //Pull In token in
         bool isToken0 = _tokenIn == address(token0);
-        (IERC20 tokenIn, IERC20 tokenOut, uint reserveIn, uint reserveOut) = isToken0
-            ? (token0, token1, reserve0, reserve1)
-            : (token1, token0, reserve1, reserve0);
+        (
+            IERC20 tokenIn,
+            IERC20 tokenOut,
+            uint reserveIn,
+            uint reserveOut
+        ) = isToken0
+                ? (token0, token1, reserve0, reserve1)
+                : (token1, token0, reserve1, reserve0);
 
         tokenIn.transferFrom(msg.sender, address(this), _amountIn);
 
         //Calc token out and fees, fee 0.3%
         uint amountInWithFee = (_amountIn * 997) / 1000;
-        amountOut = (reserveOut * amountInWithFee) / (reserveIn + amountInWithFee);
+        amountOut =
+            (reserveOut * amountInWithFee) /
+            (reserveIn + amountInWithFee);
 
         //Transfer token out to msg.sender
         tokenOut.transfer(msg.sender, amountOut);
 
         //Update reserves
+        _update(
+            token0.balanceOf(address(this)),
+            token1.balanceOf(address(this))
+        );
+    }
+
+    function addLiquidity(
+        uint _amount0,
+        uint _amount1
+    ) external returns (uint shares) {
+        //Pull In token0 and token1
+        token0.transferFrom(msg.sender, address(this), _amount0);
+        token1.transferFrom(msg.sender, address(this), _amount1);
+
+        // dy / dx = y / x
+        if (reserve0 > 0 || reserve1 > 0) {
+            require(
+                reserve0 * _amount1 == reserve1 * _amount0,
+                "dy / dx 1= y / x"
+            );
+        }
+
+        //Mint shares
+        //f(x, y) = value of liquidity = sqrt(xy)
+        // s = dx / x * T = dy / y * T
+        if (totalSupply == 0) {
+            shares = _sqrt(_amount0 * _amount1);
+        } else {
+            shares = _min(
+                (_amount0 * totalSupply) / reserve0,
+                (_amount1 * totalSupply) / reserve1
+            );
+        }
+
+        require(shares > 0, "shares = 0");
+        _mint(msg.sender, shares);
+
+        //Update reserves
         _update(token0.balanceOf(address(this)), token1.balanceOf(address(this)));
     }
 
-    function addLiquidity() external {
+    function removeLiquidity() external {}
 
+    function _sqrt(uint y) private pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
     }
 
-    function removeLiquidity() external {
-
+    function _min(uint x, uint y) private pure returns (uint) {
+        return x <= y ? x : y;
     }
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,16 @@
+// @ts-ignore
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const AMM = await ethers.getContractFactory("AMM");
+  const amm = await AMM.deploy();
 
-  await lock.deployed();
+  await amm.deployed();
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `AMM deployed to ${amm.address}`
   );
 }
 
