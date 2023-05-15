bcc6ce4f34e37946af25bf31a05ddd0715bd7d9a
Paul Simroth <paul.simroth@proton.me>
2023-05-15 15:35:30 +0200

    state variables and functions

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -4,5 +4,39 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract AMM {
+    IERC20 public immutable token0;
+    IERC20 public immutable token1;
 
+    uint public reserve0;
+    uint public reserve1;
+
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+
+    constructor(address _token0, address _token1) {
+        token0 = IERC20(_token0);
+        token1 = IERC20(_token1);
+    }
+
+    function _mint(address _to, uint _amount) private {
+        balanceOf[_to] += _amount;
+        totalSupply += _amount;
+    }
+
+    function _burn(address _from, uint _amount) private {
+        balanceOf[_from] -= _amount;
+        totalSupply -= _amount;
+    }
+
+    function swap() external {
+
+    }
+
+    function addLiquidity() external {
+
+    }
+
+    function removeLiquidity() external {
+        
+    }
 }
